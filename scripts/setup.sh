6ba3f26de2c695ec0b5148c82f2d90b8f881dc84
blacktokki <39031723+blacktokki@users.noreply.github.com>
2023-08-01 09:59:43 +0000

    feat : git config script 분리

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,25 +1,20 @@
 #!/bin/sh
 
 # Usage:
-# bash scripts/setup.sh param1 param2 param3 param4
-# * param1: github username
-# * param2: database username
-# * param3: database password, secret key
+# bash scripts/setup.sh param1 param2
+# * param1: database username
+# * param2: database password, secret key
 
 DATASOURCE_FILE=./account-service/src/main/resources/datasource.properties
 MYSQL_CONFIG_FILE=/etc/mysql./my.cnf
 SECRET_FILE=./account-service/src/main/resources/secret.properties
 FRONTEND_DIRECTORY=./frontend
 
-git config user.email "$1@github.com"
-git config user.name "$1"
-git config credential.helper store
-
 sudo apt-get update
 sudo apt-get --assume-yes install mysql-server-5.7
 sudo mysql -u root -p" " -e"
-create user '$2'@'%' identified by '$3';
-GRANT ALL PRIVILEGES ON *.* TO '$2'@'%';
+create user '$1'@'%' identified by '$2';
+GRANT ALL PRIVILEGES ON *.* TO '$1'@'%';
 flush privileges;
 SET character_set_server = 'utf8';
 create database db1_account;
@@ -29,9 +24,9 @@ sudo apt-get --assume-yes install openjdk-11-jdk
 sudo iptables -t nat -A PREROUTING -p tcp --dport 80 -j REDIRECT --to-port 9000
 
 echo "spring.datasource.url=jdbc:mysql://127.0.0.1:3306/?rewriteBatchedStatements=true&characterEncoding=UTF-8" > $DATASOURCE_FILE  # &profileSQL=true&logger=Slf4JLogger&maxQuerySizeToLog=999999
-echo "spring.datasource.username=$2" >> $DATASOURCE_FILE
-echo "spring.datasource.password=$3" >> $DATASOURCE_FILE
-echo "jwt.secret=$3" >> $SECRET_FILE
+echo "spring.datasource.username=$1" >> $DATASOURCE_FILE
+echo "spring.datasource.password=$2" >> $DATASOURCE_FILE
+echo "jwt.secret=$2" >> $SECRET_FILE
 
 ## replication DB
 echo "[mysqld]" >> $MYSQL_CONFIG_FILE
