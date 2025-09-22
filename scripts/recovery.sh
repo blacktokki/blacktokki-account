cb0317fb713c542e71cf5f83ec53f5aee70696ba
blacktokki <39031723+blacktokki@users.noreply.github.com>
2025-09-22 14:49:43 +0000

    chore : gateway ssl setup

--- scripts/recovery.sh
+++ scripts/recovery.sh
@@ -4,4 +4,5 @@
 # bash scripts/recovery.sh
 
 sudo iptables -t nat -A PREROUTING -p tcp --dport 80 -j REDIRECT --to-port 9000
+sudo iptables -t nat -A PREROUTING -p tcp --dport 443 -j REDIRECT --to-port 9000
 bash scripts/runserver.sh discovery gateway account
